f4976b3f3aa4965569d53adaad8fa8605384f181
Petra Bočková <Wickky@seznam.cz>
2022-03-22 20:59:07 +0100

    develop

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
-
+/*
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupně řada 0 až 10');
@@ -10,7 +10,7 @@ console.log('První příklad - vzestupně řada 0 až 10');
 for (let i = 0; i <= 10; i++) {
     console.log(i);
 }
-
+*/
 console.log('První příklad - použití while');
 
 let i = 0
@@ -23,7 +23,7 @@ while (i < 10) {
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
-
+/*
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
 //TADY NAPIS CYKLUS
@@ -31,7 +31,7 @@ console.log('Druhý příklad - rozdíl mezi <= a <');
 for (let k = 0; k < 10; k++) {
     console.log(k);
 }
-
+*/
 console.log('Druhý příklad - použití while');
 
 let k = 0
@@ -43,13 +43,13 @@ while (k < 10) {
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
-console.log('Třetí příklad - sestupná řada 10 - 0');
+/*console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
 for (let j = 10; j >= 0; j--) {
     console.log(j);
 }
-
+*/
 console.log('Třetí příklad - použití while');
 
 let j = 11;
