3df2dad6864b9dacd8d8837d11f9244d206f7685
Petra Bočková <Wickky@seznam.cz>
2022-03-22 19:15:07 +0100

    cviceni for

--- main.js
+++ main.js
@@ -29,3 +29,38 @@ console.log('Třetí příklad - sestupná řada 10 - 0');
 //TADY NAPIS CYKLUS
 
 console.log('-------------------');
+
+
+
+
+/*
+
+let a = 0;
+while (a < 10) {
+    console.log(a);
+    a++;
+}
+
+
+
+
+function test() {
+    console.log("před cyklem");
+    for (let i = 0; i < 10; i = i + 1) {
+        if (i > 10) { break; }
+        console.log(i);
+    }
+    console.log("za cyklem");
+}
+
+
+
+let castTela = ["h", "l", "a", "v", "a"];
+
+while (castTela.length > 3) {
+    castTela.pop();
+    console.log(castTela);
+
+}
+
+*/
